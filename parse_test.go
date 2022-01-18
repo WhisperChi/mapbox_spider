e240240141497e988c73ba86f0a81b7690c601b0
WhisperChi <xiaoyan.chi.2017@gmail.com>
2022-01-18 09:24:38 +0800

    modify test file path

--- parse_test.go
+++ parse_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestGetParamsFromFile(t *testing.T) {
 	var params Params
-	var FILE_URL = "../config-example.json"
+	var FILE_URL = "./config-example.json"
 	GetParamsFromFile(FILE_URL, &params)
 	if params.Token != "<tokenToBeReplaced>" {
 		t.Error("getToken failed")
